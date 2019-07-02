16ef87df8b1ccb2c6c17bdddfc6ca7f79b5c407e
578399797 <578399797@qq.com>
2019-07-02 14:37:23 +0800

    初次提交

---
+++ demo0328/src/main/java/Servlet/MyServlet.java
@@ -0,0 +1,31 @@
+package Servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class MyServlet implements Servlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
